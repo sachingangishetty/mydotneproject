2fa0715d6d83f571fec0603955a7e4466469385f
sachingangishetty <46860273+sachingangishetty@users.noreply.github.com>
2019-01-21 15:30:32 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jenkins_sample_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("this is sample jenkin project");
+            Console.ReadLine();
+        }
+    }
+}
